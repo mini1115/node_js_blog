8e34efeb74f3aba7a6d2b7afe5c31f4cc2081cfc
mini1115 <kmini9611@naver.com>
2024-03-07 18:03:58 +0900

    feat : render 경로 수정

--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
 
 router.get("/home", (req, res) => {
-    res.render("hello index",{layout:mainLayout});
-})
+    res.render("index", { layout: mainLayout });
+});
 router.get("/about", (req, res) => {
-    res.render("hello about",{layout:mainLayout});
-})
+    res.render("about", { layout: mainLayout });
+});
 module.exports = router;
