8f4bfb47d765dea26739fd12b2f059e8c8302f08
mini1115 <kmini9611@naver.com>
2024-03-07 18:06:56 +0900

    feat : html title 수정

--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
 
-router.get("/home", (req, res) => {
-    res.render("index", { layout: mainLayout });
+router.get(["/home","/"], (req, res) => {
+    const locals = {
+        title : "Home"
+    };
+    res.render("index", { locals, layout: mainLayout });
 });
 router.get("/about", (req, res) => {
-    res.render("about", { layout: mainLayout });
+    const locals = {
+        title : "About"
+    };
+    res.render("about", { locals,layout: mainLayout });
 });
 module.exports = router;
