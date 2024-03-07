4d4c36aafb0de7ceab0f180d83a39ac76e63d5fc
mini1115 <kmini9611@naver.com>
2024-03-07 17:53:17 +0900

    [Add] main& about ejs 파일 추가

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(expressLayouts);
+app.use("view engine","ejs");
+app.use("views","./views");
+
 
 app.use("/",require("./routes/main"));
 
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
 
-router.get("/", (req, res) => {
-    res.send("hello index");
+router.get("/home", (req, res) => {
+    res.render("hello index",{layout:mainLayout});
 })
-
+router.get("/about", (req, res) => {
+    res.render("hello about",{layout:mainLayout});
+})
 module.exports = router;
