267876b41c70fc9894784d2a0c1384d089e7dac5
mini1115 <kmini9611@naver.com>
2024-03-07 17:33:48 +0900

    [Add] route 파일 생성

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("hello index");
-})
+app.use("/",require("./routes/main"));
+
 app.listen(port,()=>{
     console.log(`app exec on port ${port}`);
 })
