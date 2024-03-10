58e7a30380d3bd7e3629b3d6fb4b57e5e7f35cb7
mini1115 <kmini9611@naver.com>
2024-03-10 16:55:23 +0900

    feat : 관리자 로그인 기능 추가

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const connectDB = require("./config/db");
 
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -15,6 +17,10 @@ app.set("views", "./views");
 app.use(express.static("public"));
 //url parser
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//webtoken
+app.use(cookieParser());
 
 app.use("/",require("./routes/main"));
 app.use("/",require("./routes/admin"));
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,9 @@ const adminLayout = "../views/layouts/admin";
 const adminLayout2 = "../views/layouts/admin-nologout";
 const User = require("../models/User");
 const bcrpyt = require("bcrypt");
+//웹토큰 비밀키
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
 
 router.get("/admin", (req, res) => {
     const locals = {
@@ -14,20 +17,41 @@ router.get("/admin", (req, res) => {
     res.render("admin/index", { locals, layout: adminLayout2 });
 });
 
-//관리자 등록 get `방식 처리
-// router.get("/register",asyncHanlder(async(req,res)=>{
-//     res.render("admin/index", {layout: adminLayout2 });
+//Check Login
+router.post("/admin", asyncHanlder(async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    // if (username === "admin" && password === "admin") {
+    //   res.send("Success");
+    // } else {
+    //   res.send("Fail");
+    // }
+    if (!user) {
+        return res.status(401).json({ message: "일치하는 사용자가 없습니다" });
+    }
+    const isValidPassword = await bcrpyt.compare(password, user.password);
+    if (!isValidPassword) {
+        return res.status(401).json({ message: "비밀번호가 일치하지 않습니다" });
+    }
+    const token = jwt.sign({ id: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/allPosts");
+})
+);
+// //관리자 등록 get `방식 처리
+// router.get("/register", asyncHanlder(async (req, res) => {
+//     res.render("admin/index", { layout: adminLayout2 });
 // })
 // );
-//관리자 등록 post 방식 처리
+// // 관리자 등록 post 방식 처리
 
-// router.post("/register",asyncHanlder(async(req,res)=>{
+// router.post("/register", asyncHanlder(async (req, res) => {
 //     //res.send("register success");
 //     //비밀번호 암호화
-//     const hashedPassword = await bcrpyt.hash(req.body.password,10);
+//     const hashedPassword = await bcrpyt.hash(req.body.password, 10);
 //     const user = await User.create({
-//         username : req.body.username,
-//         password : hashedPassword,
+//         username: req.body.username,
+//         password: hashedPassword,
 //     });
 //     res.json(`${user} created !`)
 // })
